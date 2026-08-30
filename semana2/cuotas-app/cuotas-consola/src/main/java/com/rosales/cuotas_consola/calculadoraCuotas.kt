053f451136e916e232982c0e4bc9a583a7ad0bba
Magaly Rosales Porras <magaly.rosales@tecsup.edu.pe>
2026-08-30 12:17:34 -0500

    Agrega seleccion de cuotas y calculo de interes

--- semana2/cuotas-app/cuotas-consola/src/main/java/com/rosales/cuotas_consola/calculadoraCuotas.kt
+++ semana2/cuotas-app/cuotas-consola/src/main/java/com/rosales/cuotas_consola/calculadoraCuotas.kt
@@ -5,7 +5,14 @@ data class Producto(
     val precio: Double,
     val cantidad: Int
 )
-
+fun calcularInteres(numCuotas: Int): Double {
+    return when (numCuotas) {
+        6 -> 0.20
+        12 -> 0.40
+        24 -> 0.60
+        else -> 0.0
+    }
+}
 fun main() {
     println("=========================================")
     println("   CALCULADORA DE CUOTAS - TIENDA TECSUP")
@@ -21,5 +28,41 @@ fun main() {
     val cantidad = readln().toInt()
 
     val producto = Producto(nombreProducto, precio, cantidad)
-    println("Producto registrado: ${producto.nombre}, S/ ${producto.precio} x ${producto.cantidad}")
+    val montoInicial = producto.precio * producto.cantidad
+
+    var numCuotas = 0
+    var salir = false
+
+    while (numCuotas == 0 && !salir) {
+        println()
+        println("--------- ELIJA NUMERO DE CUOTAS ---------")
+        println("1. 6 cuotas   -> Interes 20%")
+        println("2. 12 cuotas  -> Interes 40%")
+        println("3. 24 cuotas  -> Interes 60%")
+        println("4. Salir")
+        println("---------------------------------------")
+
+        print("Seleccione una opcion: ")
+        val opcion = readln()
+
+        when (opcion) {
+            "1" -> numCuotas = 6
+            "2" -> numCuotas = 12
+            "3" -> numCuotas = 24
+            "4" -> {
+                println("Saliendo del programa...")
+                salir = true
+            }
+            else -> println("Opcion no valida. Intentelo nuevamente.")
+        }
+    }
+
+    if (salir) return
+
+    val tasaInteres = calcularInteres(numCuotas)
+
+    println()
+    println("Monto Inicial: S/ ${"%.2f".format(montoInicial)}")
+    println("Numero de cuotas seleccionadas: $numCuotas")
+    println("Tasa de interes: ${(tasaInteres * 100).toInt()}%")
 }
