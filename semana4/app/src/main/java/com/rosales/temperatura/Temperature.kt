5d3465a2007f4d855394e19106002983b3a177c5
Magaly Rosales Porras <magaly.rosales@tecsup.edu.pe>
2026-09-16 01:36:06 -0500

    feat: colorear texto segun rango de temperatura

--- semana4/app/src/main/java/com/rosales/temperatura/Temperature.kt
+++ semana4/app/src/main/java/com/rosales/temperatura/Temperature.kt
@@ -4,14 +4,19 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Temperature() {
     var temperatura by remember { mutableStateOf(20) }
-
+    val colorTexto = when {
+        temperatura > 30 -> Color.Red
+        temperatura < 10 -> Color.Blue
+        else -> Color.Black
+    }
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Text(text = "$temperatura°C")
+        Text(text = "$temperatura°C", color = colorTexto)
 
         Spacer(modifier = Modifier.height(16.dp))
 
